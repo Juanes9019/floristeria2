cb4f0061d147729d998e56264a834ae6979c80a7
Deymar <pradodeymar@gmail.com>
2024-03-13 21:09:55 -0500

    prueba de deymar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('estoy en el home!') }}
+                    {{ __('estoy en el home!, Deymar') }}
                 </div>
 
                 <div class="col-md-8">
