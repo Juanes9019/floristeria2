051fe1cd294b2d36182d5dc4441899f225429a26
DMauroP <dmauriciop0121@gmail.com>
2024-11-17 14:09:07 -0500

    Diseños de tablas

--- resources/views/livewire/compra-table.blade.php
+++ resources/views/livewire/compra-table.blade.php
@@ -139,6 +139,15 @@
                                 @endif
                             </tbody>
                         </table>
+                        <label>Páginas</label>
+                        <select wire:model.live="porPagina">
+                            <option value="10">10</option>
+                            <option value="20">20</option>
+                            <option value="50">50</option>
+                        </select>
+                        <div class="mt-3">
+                            {{ $compras->links() }}
+                        </div>
                     </div>
                 </div>
             </div>
